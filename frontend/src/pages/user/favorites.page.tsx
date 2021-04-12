9bff2315f8b1226aa6ef2dd535369d7a6236e1b3
epourgh <epourgh@gmail.com>
2021-04-12 16:55:00 -0500

    adjust favorites ui

--- frontend/src/pages/user/favorites.page.tsx
+++ frontend/src/pages/user/favorites.page.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import { Container, Table, Header, Image, Icon, Button, Label } from 'semantic-ui-react'
+import { Container, Grid, Header, Image, Icon, Button } from 'semantic-ui-react'
 import globalStyles from '../../styles/Global.module.scss'
 import { useActions } from '../../hooks/useActions';
 import { Link } from "react-router-dom";
@@ -30,32 +30,42 @@ const FavoritesPage = () => {
     }
 
     return (
-        <Container className={globalStyles.minHeight}>
-            <h2>Your favorites:</h2>
-            <Table basic='very' celled collapsing>
-                <Table.Body>
-                    {userFavoritesPage.map((item) => {
-                        return (
-                            <Table.Row key={item._id} className={globalStyles.itemSpacing}>
-                                <Table.Cell>
-                                    <Header as='h4' image>
-                                        <Image src={item.image} rounded size='mini' />
-                                        <Header.Content>
+        <Container text className={globalStyles.minHeight}>
+            
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width={14}>
+                        <h2>Your favorites:</h2>
+                    </Grid.Column>
+                    <Grid.Column width={2}>
+                    </Grid.Column>
+                </Grid.Row>
+
+                {userFavoritesPage.map((item) => {
+                    return (
+                        <Grid.Row key={item._id}>
+                            <Grid.Column width={14}>
+                                <Grid>
+                                    <Grid.Row >
+                                        <Grid.Column width={4}>
+                                            <Image src={item.image} rounded size='small' verticalAlign='middle' />
+                                        </Grid.Column>
+                                        <Grid.Column width={12}>
                                             <b><Link to={`/product/${item._id}`}>{item.name}</Link></b>
-                                            <Header.Subheader>{item.price}</Header.Subheader>
-                                        </Header.Content>
-                                    </Header>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Button icon onClick={(e) => handleUnfavoriteubmission(e, item._id)}>
-                                        <Icon name='star' className={globalStyles.starStyle} />{item._id}
-                                    </Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
-            </Table>
+                                            <p>{item.price}</p>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            </Grid.Column>
+                            <Grid.Column width={2}>
+                                <Button icon onClick={(e) => handleUnfavoriteubmission(e, item._id)}>
+                                    <Icon name='star' className={globalStyles.starStyle} />
+                                </Button>
+                            </Grid.Column>
+                        </Grid.Row>
+                    )
+                })}
+            </Grid>
         </Container>
     )
 }
