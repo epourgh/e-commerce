cad166541f5a16aef112dfb8c848133915e3040e
epourgh <epourgh@gmail.com>
2021-04-13 16:06:26 -0500

    replace react carousel with flickity

--- frontend/src/pages/home.page.tsx
+++ frontend/src/pages/home.page.tsx
@@ -7,8 +7,9 @@ import globalStyles from '../styles/Global.module.scss'
 import styles from '../styles/Home.module.scss'
 import { Link } from "react-router-dom";
 
-import "../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Flickity from 'react-flickity-component'
+import "../../node_modules/flickity/dist/flickity.min.css"; 
+
 
 const HomePage: React.FC = () => {
 
@@ -20,88 +21,101 @@ const HomePage: React.FC = () => {
         fetchProductList({}); /* eslint-disable react-hooks/exhaustive-deps */
     }, [])
 
-    return (
-        <Container className={globalStyles.minHeight}>
-            <br/>
-            <Segment>
-                <Header as='h2'>
-                    {(userInfo.data._id) ? <>Welcome, {userInfo.data.username}!</> : <>Sign In</>}
-                </Header>
-                <p>{(userInfo.data._id) ? <>Take a look at our latest items!</> : <>Thank about signing in or registering with us to take advantage of our deals!</>}</p>
-            </Segment>
-            
-            <Carousel>
-                <div>
-                    <img src="/images/electronics-carousal.png" />
-                    {/* <p className="legend">Legend 1</p> */}
-                </div>
-                <div>
-                    <img src="/images/beauty-carousal.png" />
-                    {/* <p className="legend">Legend 2</p> */}
-                </div>
-                <div>
-                    <img src="/images/toys-carousal.png" />
-                    {/* <p className="legend">Legend 3</p> */}
-                </div>
-            </Carousel>
+    const flickityOptions = {
+        initialIndex: 0,
+        freeScroll: false,
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: true,
+        groupCells: 2,
+        autoPlay: 5000,
+        pauseAutoPlayOnHover: true,
+        fullscreen: false,
+        lazyLoad: true,
+        fade: true,
+        dragThreshold: 10
+    }
 
-            <h3>Shop by category...</h3>
-            <Grid relaxed columns={5}>
-                <Grid.Column className={styles.card}>
-                    <Link to='./category/1'>
-                        <Image src='/images/electronics.png' className={styles.gridImage} />
-                        <h5>Electronics</h5>
-                    </Link>
-                </Grid.Column>
-                <Grid.Column className={styles.card}>
-                    <Link to='./category/2'>
-                        <Image src='/images/videogames.png' className={styles.gridImage} />
-                        <h5>Video games</h5>
-                    </Link>
-                </Grid.Column>
-                <Grid.Column className={styles.card}>
-                    <Link to='./category/3'>
-                        <Image src='/images/shoes.png' className={styles.gridImage} />
-                        <h5>Shoes</h5>
-                    </Link>
-                </Grid.Column>
-                <Grid.Column className={styles.card}>
-                    <Link to='./category/4'>
-                        <Image src='/images/beauty.png' className={styles.gridImage} />
-                        <h5>Beauty & Health</h5>
-                    </Link>
-                </Grid.Column>
-                <Grid.Column className={styles.card}>
-                    <Link to='./category/5'>
-                        <Image src='/images/toys.png' className={styles.gridImage} />
-                        <h5>Toys & Kids</h5>
-                    </Link>
-                </Grid.Column>
-            </Grid>
-            {
-                (viewed[0].id !== 0) 
-                ?
-                <>
-                    <h3>Recently viewed items...</h3>
-                    <Grid relaxed columns={5}>
-                        {
-                            viewed.map((viewedProduct) => {
-                                return (
-                                    <Grid.Column className={styles.card}>
-                                        <Link to={`./product/${viewedProduct.id}`}>
-                                            <Image src={viewedProduct.product.image} className={styles.gridImage} />
-                                            <h5>{viewedProduct.product.name}</h5>
-                                        </Link>
-                                    </Grid.Column>
-                                )
-                            })
-                        }
-                    </Grid>
-                </>
-                :<></>
-            }
-            <br /><br />
-        </Container>
+    return (
+        <>
+            <Flickity
+                className={'carousel'} // default ''
+                elementType={'div'} // default 'div'
+                options={flickityOptions} // takes flickity options {}
+                disableImagesLoaded={false} // default false
+                reloadOnUpdate // default false
+                static // default false
+            >
+                <img src="/images/electronics-carousal.png" />
+                <img src="/images/beauty-carousal.png" />
+                <img src="/images/toys-carousal.png" />
+            </Flickity>
+            <Container className={globalStyles.minHeight}>
+                <br/>
+                <Segment>
+                    <Header as='h2'>
+                        {(userInfo.data._id) ? <>Welcome, {userInfo.data.username}!</> : <>Sign In</>}
+                    </Header>
+                    <p>{(userInfo.data._id) ? <>Take a look at our latest items!</> : <>Thank about signing in or registering with us to take advantage of our deals!</>}</p>
+                </Segment>
+                <h3>Shop by category...</h3>
+                <Grid relaxed columns={5}>
+                    <Grid.Column className={styles.card}>
+                        <Link to='./category/1'>
+                            <Image src='/images/electronics.png' className={styles.gridImage} />
+                            <h5>Electronics</h5>
+                        </Link>
+                    </Grid.Column>
+                    <Grid.Column className={styles.card}>
+                        <Link to='./category/2'>
+                            <Image src='/images/videogames.png' className={styles.gridImage} />
+                            <h5>Video games</h5>
+                        </Link>
+                    </Grid.Column>
+                    <Grid.Column className={styles.card}>
+                        <Link to='./category/3'>
+                            <Image src='/images/shoes.png' className={styles.gridImage} />
+                            <h5>Shoes</h5>
+                        </Link>
+                    </Grid.Column>
+                    <Grid.Column className={styles.card}>
+                        <Link to='./category/4'>
+                            <Image src='/images/beauty.png' className={styles.gridImage} />
+                            <h5>Beauty & Health</h5>
+                        </Link>
+                    </Grid.Column>
+                    <Grid.Column className={styles.card}>
+                        <Link to='./category/5'>
+                            <Image src='/images/toys.png' className={styles.gridImage} />
+                            <h5>Toys & Kids</h5>
+                        </Link>
+                    </Grid.Column>
+                </Grid>
+                {
+                    (viewed[0].id !== 0) 
+                    ?
+                    <>
+                        <h3>Recently viewed items...</h3>
+                        <Grid relaxed columns={5}>
+                            {
+                                viewed.map((viewedProduct) => {
+                                    return (
+                                        <Grid.Column className={styles.card}>
+                                            <Link to={`./product/${viewedProduct.id}`}>
+                                                <Image src={viewedProduct.product.image} className={styles.gridImage} />
+                                                <h5>{viewedProduct.product.name}</h5>
+                                            </Link>
+                                        </Grid.Column>
+                                    )
+                                })
+                            }
+                        </Grid>
+                    </>
+                    :<></>
+                }
+                <br /><br />
+            </Container>
+        </>
     );
 }
 
